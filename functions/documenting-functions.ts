12fb6eaffa7b783ab0eaefe4014861eb8278a854
Anthony Urbina <aurbinava002@gmail.com>
2022-12-15 20:26:37 +0000

    fixd eslint errors

--- functions/documenting-functions.ts
+++ functions/documenting-functions.ts
@@ -6,7 +6,7 @@
  * @returns nothing
  */
 
-function makeFruitSalad(fruit1: string, fruit2: string): void {
+function makeFruitSalads(fruit1: string, fruit2: string): void {
   let salad = fruit1 + fruit2 + fruit2 + fruit1 + fruit2+ fruit1 + fruit1;
   console.log(salad);
 }
